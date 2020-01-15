ea8b0821ab55b4b398fdbe33a9c9d99f13e1eed9
ethanb42 <33261459+ethanb42@users.noreply.github.com>
2020-01-14 23:41:08 -0800

    Update Checkout.cpp

--- Checkout.cpp
+++ Checkout.cpp
@@ -2,21 +2,18 @@
 #include "Checkout.h"
 
 
+//Constructor
 Checkout::Checkout()
 {
 	total = 0;
 }
 
-
+//Deconstructor
 Checkout::~Checkout()
 {
 }
 
-void Checkout::addItemPrice(std::string item, int price)
-{
-	prices[item] = price;
-}
-
+//Adds a new item
 void Checkout::addItem(std::string item)
 {
 	std::map<std::string, int> ::iterator priceIter = prices.find(item);
@@ -28,18 +25,15 @@ void Checkout::addItem(std::string item)
 	items[item]++;
 }
 
-int Checkout::calculateTotal()
-{
-	total = 0;
-	for (std::map<std::string, int>::iterator itemIter = items.begin(); itemIter != items.end(); itemIter++) {
-		std::string item = itemIter->first;
-		int itemCount = itemIter->second;
-		calculateItem(item, itemCount);
 
-	}
-	return total;
+//Adds the price to an item
+void Checkout::addItemPrice(std::string item, int price)
+{
+	prices[item] = price;
 }
 
+
+//Applies a discount to an item in checkout
 void Checkout::addDiscount(std::string item, int numOfItems, int discountPrice)
 {
 	Discount discount;
@@ -48,6 +42,7 @@ void Checkout::addDiscount(std::string item, int numOfItems, int discountPrice)
 	discounts[item] = discount;
 }
 
+//Adds together total for multiple items
 void Checkout::calculateItem(std::string item, int itemCount)
 {
 	std::map<std::string, Discount>::iterator discountIter;
@@ -62,6 +57,7 @@ void Checkout::calculateItem(std::string item, int itemCount)
 
 }
 
+//Checks and applies discount to items
 void Checkout::calculateDiscount(std::string item, int itemCount, Discount discount)
 {
 	if (itemCount >= discount.numOfItems) {
@@ -75,4 +71,17 @@ void Checkout::calculateDiscount(std::string item, int itemCount, Discount disco
 		total += itemCount * prices[item];
 	}
 
+}
+
+//Calculates the total for checkout
+int Checkout::calculateTotal()
+{
+	total = 0;
+	for (std::map<std::string, int>::iterator itemIter = items.begin(); itemIter != items.end(); itemIter++) {
+		std::string item = itemIter->first;
+		int itemCount = itemIter->second;
+		calculateItem(item, itemCount);
+
+	}
+	return total;
 }
